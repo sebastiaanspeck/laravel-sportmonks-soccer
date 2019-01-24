4c47799662ed3aab0176e5aaf2f958fd708aff82
Sebastiaan Speck <sebastiaan.speck@deinternetjongens.nl>
2019-01-24 15:20:47 +0100

    Renamed team1 and team2 to firstTeam and secondTeam

--- tests/Requests/Head2HeadTest.php
+++ tests/Requests/Head2HeadTest.php
@@ -11,7 +11,7 @@ class Head2HeadTest extends TestCase {
      */
     public function it_gets_head_to_head_statistics_between_teams_by_ids()
     {
-        $response = SoccerAPI::head2head()->betweenTeams($this->team1Id,$this->team2Id);
+        $response = SoccerAPI::head2head()->betweenTeams($this->firstTeamId,$this->secondTeamId);
 
         $this->assertNotEmpty($response);
     }
