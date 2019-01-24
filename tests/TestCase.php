22edbe49295083b8ecea246bd84affcbd2f40378
Sebastiaan Speck <sebastiaan.speck@deinternetjongens.nl>
2019-01-24 15:39:35 +0100

    Update TestCase to try and run travis.yml

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,9 +18,6 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase {
     {
         parent::setup();
 
-        // A random account's token, replace it with a real token for testing
-        Config::set('soccerapi.api_token', 'YOUR_TEST_KEY_HERE');
-
         // Usable with an English Premier League plan
         $this->fixtureId = 10332982;
         $this->multipleFixtureIds = [10332982, 10332956];
