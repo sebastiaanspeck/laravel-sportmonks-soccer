4a915b110cdd35942f7845b7648d1dddd3fdaf9d
Sebastiaan Speck <sebastiaan.speck@deinternetjongens.nl>
2019-01-24 13:48:09 +0100

    Bookmakers is implemented by Sportmonks, so it’s supported

--- tests/Requests/BookmakerTest.php
+++ tests/Requests/BookmakerTest.php
@@ -1,7 +1,6 @@
 <?php
 use Sportmonks\SoccerAPI\Facades\SoccerAPI;
 
-//TODO: not supported yet, check when it will be implemented by sportmonks
 /**
  * @group bookmaker
  */
@@ -10,12 +9,13 @@ class BookmakerTest extends TestCase {
     /**
      * @test
      */
-//    public function it_retrieves_all_bookmakers()
-//    {
-//        $response = SoccerAPI::bookmakers()->all();
-//
-//        $this->assertNotEmpty($response->data);
-//    }
+    public function it_retrieves_all_bookmakers()
+    {
+        $response = SoccerAPI::bookmakers()->all();
+
+        $this->assertNotEmpty($response->data);
+    }
+
     /**
      * @test
      */
